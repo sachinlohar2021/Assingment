fe69a497588d7fcc516e6496b9e95c0b73a9742d
sachinlohar2021 <sachinlohar2021@gmail.com>
2023-07-08 08:44:28 +0530

    java variables

---
+++ Demo1.java
@@ -0,0 +1,17 @@
+public class Demo1 {
+
+    public static void main(String[] args) {
+        int myNum = 5;
+        System.out.println(myNum);
+        float floatVar = 5.99f;
+        System.out.println(floatVar);
+        char myLetter = 'A';
+        System.out.println(myLetter);
+        boolean myBool = true;
+        System.out.println(myBool);
+        String text = "Hello";
+        System.out.println(text);
+        double d = 20.20;
+        System.out.println(d);
+    }
+}
